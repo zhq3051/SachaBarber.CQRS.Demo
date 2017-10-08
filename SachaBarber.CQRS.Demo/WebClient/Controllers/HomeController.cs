fb06d4c94c8eea5a7a6c0417e81940d1d77d3027
zeke <zhq3051@hotmail.com>
2017-10-08 22:08:58 +0800

    add command to execute

--- SachaBarber.CQRS.Demo/WebClient/Controllers/HomeController.cs
+++ SachaBarber.CQRS.Demo/WebClient/Controllers/HomeController.cs
@@ -7,27 +7,29 @@ using SachaBarber.CQRS.Demo.Orders;
 using SachaBarber.CQRS.Demo.Orders.ReadModel;
 using System.Threading.Tasks;
 using SachaBarber.CQRS.Demo.Orders.ReadModel.Models;
+using SachaBarber.CQRS.Demo.Orders.Commands;
 
 namespace WebClient.Controllers
 {
     public class HomeController : Controller
     {
         public List<StoreItem> Items;
+        public List<Order> Orders;
         public async Task<ActionResult> Index()
         {
             try
             {
                 var orderServiceInvoker = new OrderServiceInvoker();
-                var newOrders = orderServiceInvoker.CallService(service => service.GetAllStoreItemsAsync());
-                await Task.WhenAll(newOrders);
-                Items = newOrders.Result;
+                var newItems = orderServiceInvoker.CallService(service => service.GetAllStoreItemsAsync());
+                await Task.WhenAll(newItems);
+                Items = newItems.Result;
                 return View(Items);
             }
             catch (Exception ex)
             {
                 throw ex;
             }
-            
+
         }
 
         public ActionResult About()
@@ -45,9 +47,39 @@ namespace WebClient.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddOrder()
+        public async Task<ActionResult> AddOrder()
         {
-            return Redirect("Index");
+            var random = new Random().Next(1000);
+            var id = Guid.NewGuid();
+            var orderServiceInvoker = new OrderServiceInvoker();
+            var orderCreated = await orderServiceInvoker.CallService(service => service.SendCommandAsync(
+                new CreateOrderCommand()
+            {
+                ExpectedVersion = 1,
+                Id = id,
+                Address = "Address" + random,
+                Description = "Description" + random,
+                OrderItems = new List<SachaBarber.CQRS.Demo.Orders.DTOs.OrderItem>() {
+                    new SachaBarber.CQRS.Demo.Orders.DTOs.OrderItem() {
+                         OrderId = id,
+                        StoreItemId = Guid.NewGuid(),
+                        StoreItemDescription = "storeitem description",
+                        StoreItemUrl ="http://www.baidu.com"
+                } }
+            }));
+
+            return Redirect("Order");
+        }
+
+        public async Task<ActionResult> Order()
+        {
+            var orderServiceInvoker = new OrderServiceInvoker();
+            var newOrders = orderServiceInvoker.CallService(service =>
+                                service.GetAllOrdersAsync());
+            await Task.WhenAll(newOrders);
+            Orders = newOrders.Result;
+            return View(Orders);
+            
         }
     }
 }
